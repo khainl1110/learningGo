9209be1f727206e9acba378e879eee8ad7bc9b20
Khai Nguyen <khainl1110@gmail.com>
2022-10-09 16:56:00 -0700

    A simple go web app with handler and template

--- hello/main.go
+++ hello/main.go
@@ -1,38 +1,62 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"net/http"
 
-    "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
+
+	"html/template"
 )
 
-func main() {
-    r := mux.NewRouter()
+// declaring struct
+type Action struct {
+	// defining struct fields
+	Name   string
+	Action string
+}
 
-    r.HandleFunc("/", HomeHandler)
-    r.HandleFunc("/products", ProductsHandler)
-    r.HandleFunc("/articles", ArticlesHandler)
-    http.Handle("/", r)
-   
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/products", ProductsHandler)
+	r.HandleFunc("/articles", ArticlesHandler)
+	http.Handle("/", r)
+
 	http.ListenAndServe(":80", r)
 }
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	//vars := mux.Vars(r)
-    w.WriteHeader(http.StatusOK)
-    fmt.Fprintf(w,"Home handler")
+	w.WriteHeader(http.StatusOK)
+
+	std1 := Action{"Vani", "Home handler"}
+
+	t, _ := template.ParseFiles("templates/template.html")
+
+	// web output to print merged data
+	t.Execute(w, std1)
+
 }
 
 func ProductsHandler(w http.ResponseWriter, r *http.Request) {
 	//vars := mux.Vars(r)
-    w.WriteHeader(http.StatusOK)
-    fmt.Fprintf(w, "Products handler")
+	w.WriteHeader(http.StatusOK)
+
+	std2 := Action{"Vani", "Products handler"}
+
+	t, _ := template.ParseFiles("templates/template.html")
+
+	t.Execute(w, std2)
 }
 
 func ArticlesHandler(w http.ResponseWriter, r *http.Request) {
 	//vars := mux.Vars(r)
-    w.WriteHeader(http.StatusOK)
-    fmt.Fprintf(w, "Category handler")
-}
+	w.WriteHeader(http.StatusOK)
 
+	std1 := Action{"Vani", "Articles handler"}
+
+	t, _ := template.ParseFiles("templates/template.html")
+
+	t.Execute(w, std1)
+}
